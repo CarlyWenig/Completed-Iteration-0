fa7bac11a550b0918e9c246dd3ca1fc7a8cfa245
CarlyWenig <69254490+CarlyWenig@users.noreply.github.com>
2020-08-11 11:33:51 -0400

    Add files via upload

---
+++ ToDoList/ToDoList/ToDoList/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Carly Wenig on 8/11/20.
+//  Copyright © 2020 Carly Wenig. All rights reserved.
+//
+
+import UIKit
+class ToDoClass {
+    var description = ""
+    var important = false
+}
